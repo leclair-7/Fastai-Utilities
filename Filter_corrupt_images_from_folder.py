1d079c3aea9be5ba829a8486a4f21426b892aa8a
Lucas Hagel <andromeda.t@tutanota.com>
2019-01-27 12:22:05 -0600

    Deletes corrupt images from all class folders assuming imagenet style dataset setup (1 folder per label)

--- Filter_corrupt_images_from_folder.py
+++ Filter_corrupt_images_from_folder.py
@@ -1,28 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
 
 
 from os import listdir
 from PIL import Image
-
+import os
 
 # In[12]:
 
+def delete_corrupt_images():
+    for filename in listdir('./'):
+        if filename.endswith(".jpg"):
+            try:
+                fn = "./" + filename
+                img = Image.open(fn)
+                img.verify()
+            except (IOError, SyntaxError) as e:
+                os.remove(fn)
 
-for filename in listdir('./'):
-    if filename.endswith(".jpg"):
-        try:
-            fn = "./" + filename
-            img = Image.open(fn)
-            img.verify()
-        except (IOError, SyntaxError) as e:
-            os.remove(fn)
-
-
-# In[ ]:
-
-
-
-
+# os.walk does (dirpath, dirnames, filenames)
+l = next(os.walk('.'))[1]
+#print("l=", l)
+path = os.getcwd()
+for i in l:
+    os.chdir(os.path.join(path , i ))
+    print(os.getcwd())
+    #next(os.walk('.'))[2]
+    delete_corrupt_images()
+    os.chdir("..")
